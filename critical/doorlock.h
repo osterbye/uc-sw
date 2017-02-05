6707ea78086441fbaf6f49e9429d52ccacfcc1db
Nikolaj Due Oesterbye <nikolaj@due-oesterbye.dk>
2017-02-05 16:41:22 +0100

    Switch lock/unlock relay

--- critical/doorlock.h
+++ critical/doorlock.h
@@ -3,11 +3,11 @@
 
 #include <stdbool.h>
 
-#define DOOR_LOCK_PORT gioPORTA
-#define DOOR_LOCK_NUM  0
+#define DOOR_UNLOCK_PORT gioPORTA
+#define DOOR_UNLOCK_NUM  0
 
-#define DOOR_UNLOCK_PORT gioPORTB
-#define DOOR_UNLOCK_NUM  2
+#define DOOR_LOCK_PORT gioPORTB
+#define DOOR_LOCK_NUM  2
 
 void vDoorlock (void *pvParameters);
 void doorlockRequestState(bool locked);
